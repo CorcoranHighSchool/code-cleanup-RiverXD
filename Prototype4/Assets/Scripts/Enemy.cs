c3c40bf8dd5ec3fda20f5a988be020f3e009f009
RiverXD <142622748+RiverXD@users.noreply.github.com>
2024-04-11 12:55:35 -0700

    Removed unused libraries. Added String const. Added SerializeField. Enemy.cs

--- Prototype4/Assets/Scripts/Enemy.cs
+++ Prototype4/Assets/Scripts/Enemy.cs
@@ -1,17 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 3.0f;
+    [SerializeField] private float speed = 3.0f;
     private Rigidbody enemyRb;
     private GameObject player;
-
+    private string const player = "Player";
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
+        player = GameObject.Find(player);
         enemyRb = GetComponent<Rigidbody>();
     }
 
